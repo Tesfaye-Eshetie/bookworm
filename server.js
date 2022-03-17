a63892270c47837c4b62bc52760ec0c586447f1a
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-03-16 23:00:03 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const port = process.env.PORT || 5000;
+var app = express();
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve static files from /public
+app.use(express.static(__dirname + '/public'));
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// include routes
+app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
